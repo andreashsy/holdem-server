e98f62808e9c9f0b115340576e9b5b569ba02624
unknown <songyanghan+git@gmail.com>
2023-05-30 20:59:10 +0800

    refactor consumer receive message into separate functions

--- holdem/consumers.py
+++ holdem/consumers.py
@@ -46,52 +46,62 @@ class ChatConsumer(AsyncWebsocketConsumer):
         msg_type = text_data_json['type']
         message = text_data_json['message']
         user_id = text_data_json['userId']
+        
         if not self.user_id and not msg_type == 'server_message':
             self.user_id = user_id
         
         if msg_type == 'player_join':
-            waiting_room_list = cache.get('player_waiting_room')
-            if user_id in waiting_room_list:
-                await self.send_server_message(message=user_id + ' is already in the room!')
-                return
-
-            waiting_room_list.append(user_id)
-            cache.set('player_waiting_room', waiting_room_list)
-            await self.send_waiting_room_update(message=", ".join(waiting_room_list))
+            await self.handle_player_join(user_id)
 
         elif msg_type == 'player_leave':
-            waiting_room_list = cache.get('player_waiting_room')
-            if not user_id in waiting_room_list:
-                await self.send_server_message(message=user_id + ' is not in the room!')
-                return
-
-            waiting_room_list.remove(user_id)
-            cache.set('player_waiting_room', waiting_room_list)
-            await self.send_waiting_room_update(message=", ".join(waiting_room_list))
+            await self.handle_player_leave(user_id)
 
         elif msg_type == 'start_game':
-            waiting_room_list = cache.get('player_waiting_room')
-            if len(waiting_room_list) < 2:
-                # not enough players
-                return
-            if cache.get('holdem_game'):
-                # already a game in progress
-                return
-            cache.set('player_waiting_room', [])
-
-            # update waiting list
-            await self.send_waiting_room_update(message="Game has started")
-           
-            # start game
-            players = [HoldemPlayer(stack=100, id=player_id) for player_id in waiting_room_list]
-            holdem_game = HoldemGameState(players=players)
-            for player in holdem_game.players:
-                player.participate()
-            holdem_game.start_preflop()
-            cache.set('holdem_game', holdem_game)
+            await self.handle_start_game()
             
         else:
             await self.send_chat_message(user_id, message)
+
+    async def handle_player_join(self, user_id:str):
+        waiting_room_list = cache.get('player_waiting_room')
+        if user_id in waiting_room_list:
+            await self.send_server_message(message=user_id + ' is already in the room!')
+            return
+
+        waiting_room_list.append(user_id)
+        cache.set('player_waiting_room', waiting_room_list)
+        await self.send_waiting_room_update(message=", ".join(waiting_room_list))
+
+    async def handle_player_leave(self, user_id:str):
+        waiting_room_list = cache.get('player_waiting_room')
+        if not user_id in waiting_room_list:
+            await self.send_server_message(message=user_id + ' is not in the room!')
+            return
+
+        waiting_room_list.remove(user_id)
+        cache.set('player_waiting_room', waiting_room_list)
+        await self.send_waiting_room_update(message=", ".join(waiting_room_list))
+
+    async def handle_start_game(self):
+        waiting_room_list = cache.get('player_waiting_room')
+        if len(waiting_room_list) < 2:
+            # not enough players
+            return
+        if cache.get('holdem_game'):
+            # already a game in progress
+            return
+        cache.set('player_waiting_room', [])
+
+        # update waiting list
+        await self.send_waiting_room_update(message="Game has started")
+        
+        # start game
+        players = [HoldemPlayer(stack=100, id=player_id) for player_id in waiting_room_list]
+        holdem_game = HoldemGameState(players=players)
+        for player in holdem_game.players:
+            player.participate()
+        holdem_game.start_preflop()
+        cache.set('holdem_game', holdem_game)
 
     async def send_chat_message(self, user_id:str, message:str):
         await (self.channel_layer.group_send)(
